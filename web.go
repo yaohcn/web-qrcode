12e04f0b07f1647f97cdffccbc954dbb86367d25
yaohcn <yaohua_yh@126.com>
2018-09-10 18:09:29 +0800

    fisrt commit

---
+++ web.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数，默认是不会解析的
+	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+}
+func qrcode(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("qrcode.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		// r.ParseForm()
+		fmt.Println("qrcode:", r.FormValue("qrcode"))
+		buf := bytes.NewBufferString(r.FormValue("qrcode"))
+		fmt.Printf("%d\n", len(buf.Bytes()))
+		fmt.Println(buf.String())
+	}
+}
+func qrcodeParse(qr string) {
+}
+func main() {
+	http.HandleFunc("/", sayhelloName) //设置访问的路由
+	http.HandleFunc("/qrcode", qrcode)
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
